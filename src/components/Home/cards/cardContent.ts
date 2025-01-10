5389977cc6d19367464a49592f25a02655c8eef8
Saqman77 <saqlainhaider77@gmail.com>
2025-01-11 00:58:57 +0500

    chal ja na putr

--- src/components/Home/cards/cardContent.ts
+++ src/components/Home/cards/cardContent.ts
@@ -1,42 +1,42 @@
 export const cardContent = [
     {
       id: "Video consultation call", // Same as the heading
-      imgSrc: "/src/assets/home/Frame21.svg",
+      imgSrc: "src/assets/home/Frame21.svg",
       heading: "Video consultation call",
       backgroundColor: "#7163DE80",
       description: "We offer a free, quick 30 minute video consultation call to give you clarity and offer our advice on any write-ups and content you have..."
     },
     {
       id: "Manuscript assessment",
-      imgSrc: "/src/assets/home/Thesis-pana.svg",
+      imgSrc: "src/assets/home/Thesis-pana.svg",
       heading: "Manuscript assessment",
       backgroundColor: "#E9B50080",
       description: "We can assess any manuscripts or papers for errors and mistakes, offering you the best possible content suggestions to help you excel.."
     },
     {
       id: "Editing samples",
-      imgSrc: "/src/assets/home/Learning-pana.svg",
+      imgSrc: "src/assets/home/Learning-pana.svg",
       heading: "Editing samples",
       backgroundColor: "#F0195B33",
       description: "Want a sample of our service? Fear not, just submit a text paragraph of your choice and see how the magic happens!"
     },
     {
       id: "Video consultation call", // Same as the heading
-      imgSrc: "/src/assets/home/Frame21.svg",
+      imgSrc: "src/assets/home/Frame21.svg",
       heading: "Video consultation call",
       backgroundColor: "#7163DE80",
       description: "We offer a free, quick 30 minute video consultation call to give you clarity and offer our advice on any write-ups and content you have..."
     },
     {
       id: "Manuscript assessment",
-      imgSrc: "/src/assets/home/Thesis-pana.svg",
+      imgSrc: "src/assets/home/Thesis-pana.svg",
       heading: "Manuscript assessment",
       backgroundColor: "#E9B50080",
       description: "We can assess any manuscripts or papers for errors and mistakes, offering you the best possible content suggestions to help you excel.."
     },
     {
       id: "Editing samples",
-      imgSrc: "/src/assets/home/Learning-pana.svg",
+      imgSrc: "src/assets/home/Learning-pana.svg",
       heading: "Editing samples",
       backgroundColor: "#F0195B33",
       description: "Want a sample of our service? Fear not, just submit a text paragraph of your choice and see how the magic happens!"
