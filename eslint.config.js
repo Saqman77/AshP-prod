70da95d9ddde6387e78d6c9d5e6f380f1b8ec9b1
Saqman77 <saqlainhaider77@gmail.com>
2025-01-10 23:21:56 +0500

    Update eslint.config.js

--- eslint.config.js
+++ eslint.config.js
@@ -5,6 +5,14 @@ import reactRefresh from 'eslint-plugin-react-refresh'
 import tseslint from 'typescript-eslint'
 
 export default tseslint.config(
+  {languageOptions: {
+    // other options...
+    parserOptions: {
+      project: ['./tsconfig.node.json', './tsconfig.app.json'],
+      tsconfigRootDir: import.meta.dirname,
+    },
+  },
+},
   { ignores: [] },
   {
     extends: [js.configs.recommended, ...tseslint.configs.recommended],
